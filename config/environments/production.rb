157076496dfab1742034307c5edec4272ffb900e
liliya-tagirova <l.tagirova@edstein.ru>
2023-08-26 15:38:54 +0300

    her

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << /\.herokuapp\.com\z/
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.hosts << /\.herokuapp\.com\z/
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
