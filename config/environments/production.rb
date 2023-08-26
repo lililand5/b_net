0866e880cec6607be1b0ef43a5f65e4235839d9c
liliya-tagirova <l.tagirova@edstein.ru>
2023-08-26 14:26:07 +0300

    env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.hosts << "my-network-17bd33be55c0.herokuapp.com"
+  config.hosts << "my-network-f10907b897e2.herokuapp.com"
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
